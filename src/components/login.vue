aa41bc2751261ac1c1178ac5e39bd3f8196b75b6
gitNYB <nyblww@163.com>
2017-03-31 17:04:19 +0800

    first commit

---
+++ src/components/login.vue
@@ -0,0 +1,55 @@
+<template>
+<el-form ref="form" :model="form" label-width="80px">
+  <el-form-item label="用户名">
+    <el-input v-model="form.username"></el-input>
+  </el-form-item>
+  <el-form-item label="密码">
+    <el-input v-model="form.password"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="login">登陆</el-button>
+    <el-button @click="alert">取消</el-button>
+  </el-form-item>
+</el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form:{
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    alert(){
+      alert('111111')
+    },
+    login() {
+       // 获取已有账号密码
+       this.$http.get('/api/login/getAccount').then(
+         (response) => {
+           console.log(response)
+           let params = {
+             username: this.form.username,
+             password: this.form.password
+           }
+           return this.$http.post('/api/login/createAccount', params)
+         }
+       ).then(
+         (response) => {
+           console.log(response)
+         }
+       ).catch(
+         (reject) => {
+           console.log(reject)
+         }
+       )
+       }
+  }
+}
+</script>
+<style lang="css">
+</style>
